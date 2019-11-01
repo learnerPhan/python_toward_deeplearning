81037a69a550ada3d64b854ecdaf33ce4a5b9f7a
learnerPhan <2yearsff@gmail.com>
2019-11-01 17:03:54 +0700

    fix simulation data

--- learn_file_numpyarray/package/simu_data/data_cifar10.py
+++ learn_file_numpyarray/package/simu_data/data_cifar10.py
@@ -17,11 +17,41 @@ train_batch_data = []
 train_batch_data.append(Image0)
 train_batch_data.append(Image1)
 
-train_batch[0]['data'] = np.array(train_batch_data)
-train_batch[0]['labels'] = np.array([Class0, Class1])
+train_batch[0]['data'] = np.array([Image0, Image1])
+train_batch[0]['labels'] = [Class0, Class1]
 
 train_batch[1]['data'] = np.array([Image2, Image3])
-train_batch[1]['labels'] = np.array([Class2, Class2])
+train_batch[1]['labels'] = [Class2, Class2]
 
 test_batch['data'] = np.array([Image2, Image3])
-test_batch['labels'] = np.array([Class2, Class2])
+test_batch['labels'] = [Class2, Class2]
+
+#RQ1 : 2 training batches, 1 testing batch
+if 0:
+	print(len(train_batch))
+	print(len(test_batch))
+
+#RQ2 : each batch has 2 images and 2 corresponding classes
+#RQ3 : each batch has 2*18 for 2 images of size 18 = 2rows * 3columns * 3channelsRGB
+#RQ4 : each data batch is of dictionary structure, having key = {'data', 'labels'}
+	   #value of key 'data' is a numpy array
+	   #value of key 'labels' is a list
+if 0:	
+	print(type(train_batch[0]['data']))
+	print(type(train_batch[0]['labels']))
+	
+	print(type(train_batch[1]['data']))
+	print(type(train_batch[1]['labels']))
+	
+	print(type(test_batch['data']))
+	print(type(test_batch['labels']))
+
+
+	print(train_batch[0]['data'].shape)
+	print(len(train_batch[0]['labels']))
+	
+	print(train_batch[1]['data'].shape)
+	print(len(train_batch[1]['labels']))
+	
+	print(test_batch['data'].shape)
+	print(len(test_batch['labels']))
