4558d9ec2ba91e41768d7c0e322ba9d541a938b0
learnerPhan <2yearsff@gmail.com>
2019-12-29 15:44:31 +0700

    compute gradient of the loss function

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -29,6 +29,10 @@ def svm_loss_naive(W, X, y, reg):
     loss = 0.0
     for i in range(num_train):
         scores = X[i].dot(W)
+        """
+        print (scores.shape)
+        scores.shape = (10,)
+        """
         correct_class_score = scores[y[i]]
         for j in range(num_classes):
             if j == y[i]:
@@ -36,13 +40,17 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:, j] += X[i]
+                dW[:, y[i]] -= X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
+    dW /= num_train
 
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
+    dW += reg*2*W
 
     #############################################################################
     # TODO:                                                                     #
